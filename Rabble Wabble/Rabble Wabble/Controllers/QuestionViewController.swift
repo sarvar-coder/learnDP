bfd944ae2eba6d3e4a695a3d78bda6989ad1e06f
Sarvar019 <megafon.kcr.09.09@gmail.com>
2024-12-12 13:05:58 +0500

    initial commit

---
+++ Rabble Wabble/Rabble Wabble/Controllers/QuestionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionViewController.swift
+//  Rabble Wabble
+//
+//  Created by Sarvar Boltaboyev on 12/12/24.
+//
+
+import UIKit
+
+public class QuestionViewController: UIViewController {
+    
+}
+
---
+++ Rabble Wabble/Rabble Wabble/Models/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  Rabble Wabble
+//
+//  Created by Sarvar Boltaboyev on 12/12/24.
+//
+
+import Foundation
